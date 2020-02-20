acc2f824be5d3af6bb162143df670c73d2ac4336
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 15:08:20 +0530

    Initial commit

---
+++ fullname/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstndlastname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fullname("Supraja", "Baratam");
+        }
+
+        static void fullname(string fn, string ln)
+        {
+            string s = string.Concat(fn, " ", ln);
+            Console.WriteLine(s); 
+        }
+    }
+}
